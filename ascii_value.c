22080373390dad675959e1893653e84dfdd92dc6
sonu kumar <sonukhg0123@gmail.com>
2026-09-03 23:18:59 +0530

    Add files via upload
    Hands-on practice exploring standard ASCII values in C. Manually mapped and verified integer values for lowercase/uppercase alphabets, special characters, mathematical operators, brackets, and punctuation marks.

---
+++ ascii_value.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+int main() {
+//"in this sequence, the value increased by one from a to z"
+char aa,ab,ac,ad,ae;
+aa='a';
+printf("ascii value of a is %d",aa);
+ab='b';
+printf("\n ascii value of b is %d",ab);
+ac='c';
+printf("\n ascii value of c is %d",ac);
+ad='d';
+printf("\n ascii value of d is %d",ad);
+ae='e';
+printf("\n ascii value of e is %d",ae);
+//"in this sequence, the value increased by one from A to Z"
+char AA='A';
+printf("\n ascii value of A is %d",AA);
+char AB='B';
+printf("\n ascii value of B is %d",AB);
+char AC,AD,AE;
+AC='C';
+printf("\n ascii value of C is %d",AC);
+AD='D';
+printf("\n ascii value of D is %d",AD);
+AE='E';
+printf("\n ascii value of E is %d",AE);
+//any special characters ascii values
+char ds,rs,ar,ht,as,us,hd,pb,ts;
+ds='$';
+printf("\n ascii value of '$' is: %d",ds);
+rs='₹';
+printf("\n ascii value of '₹' is: %d",rs);
+ar='@';
+printf("\n ascii value of '@' is: %d",ar);
+ht='#';
+printf("\n ascii value of '#' is: %d",ht);
+as='&';
+printf("\n ascii value of '&' is: %d",as);
+us='_';
+printf("\n ascii value of '_' is: %d",us);
+hd='-';
+printf("\n ascii value of '-' is: %d",hd);
+pb='|';
+printf("\n ascii value of '|' is: %d",pb);
+ts='~';
+printf("\n ascii value of '~' is: %d",ts);
+//ascii value of math and coding symbole
+char ps,ms,ss,fs,bs,es,prs,cs;
+ps='+';
+printf("\n ascii value of '+' is: %d",ps);
+ms='_';
+printf("\n ascii value of '_' is: %d",ms);
+ss='*';
+printf("\n ascii value of '*' is: %d",ss);
+fs='/';
+printf("\n ascii value of '/' is: %d",fs);
+bs='\\';
+printf("\n ascii value of '\\' is: %d",bs);
+es='=';
+printf("\n ascii value of '=' is: %d",es);
+prs='%';
+printf("\n ascii value of 'percentage sign' is: %d",prs);
+cs='^';
+printf("\n ascii value of '^' is: %d",cs);
+//ASCII value of brackets and Enclosures
+char op,clp,ob,cb,lt,gt;
+op='(';
+printf("\n ascii value of '(' is: %d",op);
+clp=')';
+printf("\n ascii value of ')' is: %d",clp);
+ob='[';
+printf("\n ascii value of '[' is: %d",ob);
+cb=']';
+printf("\n ascii value of ']' is: %d",cb);
+lt='<';
+printf("\n ascii value of '<' is: %d",lt);
+gt='>';
+printf("\n ascii value of '>' is: %d",gt);
+//ASCII value of punctuation & quotes
+char dp,com,clm,sem,que,ema,sq,dq;
+dp='.';
+printf("\n ascii value of '.' is: %d",dp);
+com=',';
+printf("\n ascii value of ',' is: %d",com);
+clm=':';
+printf("\n ascii value of ':' is: %d",clm);
+sem=';';
+printf("\n ascii value of ';' is: %d",sem);
+que='?';
+printf("\n ascii value of '?' is: %d",que);
+ema='!';
+printf("\n ascii value of '!' is: %d",ema);
+sq='\'';
+printf("\n ascii value of '\'' is: %d",sq);
+dq='"';
+printf("\n ascii value of '\"' is: %d",dq);
+    return 0;
+}
