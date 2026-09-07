5ca37ab6dd1865fafdbdf27fcd564bfde41fb7f2
sonu kumar <sonukhg0123@gmail.com>
2026-09-07 19:22:15 +0530

    Add files via upload

--- new.c
+++ new.c
@@ -1,57 +1,15 @@
-#include <stdio.h>
-#include <conio.h>
-#include <windows.h>
-
-int main() {
-    int x = 10, y = 10;
-    char ch;
-
-    while (1) {
-        system("cls");
-
-        // Game Area
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 40; j++) {
-
-                if (i == y && j == x)
-                    printf("P");   // Player
-
-                else
-                    printf(".");
-            }
-            printf("\n");
-        }
-
-        printf("\nUse W A S D keys to move\n");
-        printf("Press Q to quit\n");
-
-        ch = getch();
-
-        if (ch == 'w' || ch == 'W')
-            y--;
-
-        else if (ch == 's' || ch == 'S')
-            y++;
-
-        else if (ch == 'a' || ch == 'A')
-            x--;
-
-        else if (ch == 'd' || ch == 'D')
-            x++;
-
-        else if (ch == 'q' || ch == 'Q')
-            break;
-
-        // Boundary
-        if (x < 0) x = 0;
-        if (x > 39) x = 39;
-        if (y < 0) y = 0;
-        if (y > 19) y = 19;
-
-        Sleep(50);
-    }
-
-    printf("Game Over!\n");
-
-    return 0;
+#include<stdio.h>
+int main(){
+// Q-2 delivery charge if cart total >= 500, print "free delivery"; otherwise,add a
+// 150 fee and print the new total.
+int c;
+printf("enter cart total:");
+scanf("%d",&c);
+if(c>=500){
+    printf("free delivery");
+}
+else if(c<=499){
+    printf("new total:%d",c+150);
+}
+return 0;
 }
